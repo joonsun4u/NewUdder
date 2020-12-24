3942a6ae5c4a98c071487eb143ee16f805ede271
Joon-Sun Kim <joonsun4u@gmail.com>
2020-12-24 14:09:34 -0800

    delelte a viewDidDisappear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -14,9 +14,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        //
-    }
 
 
 }
