aef50cb952356b07c7aaa90181938c8093942ff7
Joon-Sun Kim <joonsun4u@gmail.com>
2020-12-24 13:28:55 -0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Joon-Sun Kim on 12/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
